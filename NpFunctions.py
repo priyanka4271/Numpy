7f594bd4bb48eb34a8b9b0c330432562c54d8f5a
priyanka4271 <ps9673932@gmail.com>
2025-03-30 11:56:12 +0530

    add new file

---
+++ NpFunctions.py
@@ -0,0 +1,10 @@
+import numpy as np
+c=np.array([
+    [[1,2],[3,4]],
+ [[5,6],[7,8]]
+           ])
+print("\n Original #D array:")
+print(c)
+a=np.array([[1,2,3,],[4,5,6]])
+flat_a=a.ravel()
+print(flat_a)
